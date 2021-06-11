845bc90d11cb5a45a4624845bf1dc64e5c75db5c
Michał Wójtowicz <wojtowiczmichal97@gmail.com>
2021-06-11 19:27:49 +0200

    Initial

---
+++ StudentUEK-SwiftUI/StudentUEK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentUEK_SwiftUIApp.swift
+//  StudentUEK-SwiftUI
+//
+//  Created by private on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct StudentUEK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
